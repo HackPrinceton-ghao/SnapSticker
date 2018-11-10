d2aa813c0ac2364d2b9af41eb099df731fe4003d
G Hao Lee <ghao269@hotmail.com>
2018-11-10 00:45:37 -0500

    drawing app

---
+++ SnapStickerTests/SnapStickerTests.swift
@@ -0,0 +1,36 @@
+//
+//  SnapStickerTests.swift
+//  SnapStickerTests
+//
+//  Created by G Hao Lee on 11/10/18.
+//  Copyright © 2018 haoleeg. All rights reserved.
+//
+
+import XCTest
+@testable import SnapSticker
+
+class SnapStickerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
